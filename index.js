7bd8de5d0508ec36212def84768ce275210e0f40
Wares <fazelyar.wares@gmail.com>
2020-04-11 04:21:46 +0000

    implemented classes to commit transactions like withdrawls and deposits to an account

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
 
-  constructor(amount) {
+}
+
+// let balance = 500.00;
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
   }
 
 }
 
 
-
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+// console.log('Transaction 1:', t1)
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+// const t2 = new Withdrawal(9.99, myAccount);
+// t2.commit();
+// // console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(50.25, myAccount);
+t3.commit();
+// console.log('Transaction 1:', t3);
+
+console.log('Ending Balance:', myAccount.balance);
+
+// console.log('Balance:', balance);
